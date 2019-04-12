753956b10696e609ea3d5324657f147d7628a38b
wentworth-oxfam <chris.wentworth@oxfam.org>
2019-04-12 12:05:33 -0400

    push all

--- backend/js/pubDir.js
+++ backend/js/pubDir.js
@@ -1,1 +1,1 @@
-var pubDir = ['a', 'b', 'c', 'oxfam-web-analytics', 'test-chris-oxa', 'organic-main-donation-cr', 'email-deliverability', 'daily-organic-traffic', 'website-content']
+var pubDir = ['a', 'b', 'oxfam-web-analytics', 'test-chris-oxa', 'organic-main-donation-cr', 'email-deliverability', 'daily-organic-traffic', 'website-content']
